aa083f6bfabbc794de49c0e82c95e3504caa9a69
PUGALENTHIG <122016152+PUGALENTHIG@users.noreply.github.com>
2023-03-14 08:55:10 +0530

    Add files via upload

---
+++ login.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>welcome</title>
+<link rel ="stylesheet" type ="text/css" href="Untitled-2.css">
+
+</head>
+
+<body>
+<div id="header"><img src="logo.jpg" width="230" height="302" /><img src="head.png" width="670" height="303" /></div>
+<div id="menu">
+<center>
+<ul id="list">
+<li><a href="login.php">Sign In</li>
+<li><a href="index.php">Home</li>
+<li><a href="about.php">About as</li>
+<li><a href="contect.php">contect</li>
+</ul>
+</center>
+</div>
+<p id="currentTime"></p>
+      <script src="script.js"></script>
+       <form action="action_page.php" method="post">
+  <div class="imgcontainer">
+    <img src="img_avatar2.png" alt="Avatar" height="54" class="avatar">  </div>
+
+  <div class="container">
+    <label for="uname"><b>Username</b></label>
+    <input type="text" placeholder="Enter Username" name="uname" required>
+
+    <label for="psw"><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="psw" required>
+
+    <button type="submit">Login</button>
+    <label>
+      <input type="checkbox" checked="checked" name="remember"> Remember me
+    </label>
+  </div>
+
+  <div class="container" style="background-color:#f1f1f1">
+
+	  <span class="new"><a href="new user.php">NEW USER</a></span>
+    <span class="psw">Forgot <a href="#">password?</a></span>
+  </div>
+</form>
+     
+      <button>
+      <p>      
+      <p>      
+      <p>
+            <input type="submit" name="submit" id="submit" value="submit" />
+        </button>
+        <button>
+        <input type="reset" name="cansel" id="submit" value="cansel" />
+        </button> 
+</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <p>&nbsp;</p>
+      <div id="end">
+<center>
+<h4>copyrights &copy; 48,KUMAR STREET,GOBICHETTIPALAYAM,ERODE-638506,</h4>
+<h5>PHONE NO-6383106107,MAIL ID-pugalpugalee333@gmail.com</h5>
+</center>
+</div>
+
+</body>
+
+</html>
